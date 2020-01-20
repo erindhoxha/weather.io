a79bab4cb72eb096f9e22b83a4bc1a375db1f58f
Erind Hoxha <erind_cbh@hotmail.com>
2020-01-21 02:48:05 +1300

    birds

--- js/main.js
+++ js/main.js
@@ -14,7 +14,13 @@ weatherApp.config(function($routeProvider) {
 })
 
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope','cityService','$http', '$resource', function($scope, cityService, $http, $resource) {
+weatherApp.controller('homeController', ['$scope','cityService','$http', '$resource', '$timeout', function($scope, cityService, $http, $resource, $timeout) {
+    $timeout(function() {
+        var birds = document.getElementsByClassName("pajaro");
+        angular.forEach(birds, function(bird) {
+          bird.classList.add('hide');
+        });
+    }, 0)
     $scope.key = '886705b4c1182eb1c69f28eb8c520e20';
     $scope.example = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&APPID=' + $scope.key;
     $scope.city = cityService.city;
