731b98e64962e8d2faadfa7b4462f22a74790ec6
Erind Hoxha <erind.hoxha@theta.co.nz>
2020-01-21 18:20:24 +1300

    added interaction animation loading

--- js/controllers/weatherController.js
+++ js/controllers/weatherController.js
@@ -10,41 +10,35 @@ angular.module('weatherApp').controller('weatherController', ['$scope','cityServ
         });
     }, 0)
 
-    $scope.currentDate = new Date().getDay();
-
     // key, example of the API call
     $scope.key = '886705b4c1182eb1c69f28eb8c520e20';
     $scope.example = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=Auckland&units=metric&cnt=7&APPID=' + $scope.key;
     $scope.city = cityService.city;
     $scope.length = cityService.getLength();
 
+    $scope.currentDay = new Date().getDay();
     $scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     $http.get($scope.example)
     .success(function(response) {
         $scope.daysTemperatures = response.list;
+
         const hours = new Date().getHours()
         const isDayTime = hours > 6 && hours < 20
         if (isDayTime) {
-            $scope.celsius = Math.floor(response.list[$scope.currentDate].temp.day);
+            $scope.celsius = Math.floor(response.list[$scope.currentDay].temp.day);
         } else {
-            $scope.celsius = Math.floor(response.list[$scope.currentDate].temp.night);
+            $scope.celsius = Math.floor(response.list[$scope.currentDay].temp.night);
         }
     })
+
     .error(function(e) {
         console.log(e);
     })
 
-    // If you want to change the API Values
-    // Resource helps if you want to change the arguments q: or cnt: in angularJS
-    $scope.weatherAPI =
-    $resource($scope.example, 
-        {
-            callback: "JSON_CALLBACK" 
-        },
-        {
-            get: { method: "JSONP" }
-        });
-    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: 2});
+    
+    $scope.convertToDate = function(dt) {
+        return new Date(dt * 1000);
+    }
 
 }])
