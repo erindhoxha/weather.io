44d5202af8c873a24060d8d02d80e2dbfeeb5ad5
Erind Hoxha <erind.hoxha@theta.co.nz>
2020-01-21 16:09:04 +1300

    added down arrow

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,12 +1,18 @@
 // CONTROLLERS
 
 angular.module('weatherApp').controller('homeController', ['$scope','cityService','$http', '$resource', '$timeout', function($scope, cityService, $http, $resource, $timeout) {
+
+    // hide birds, when loaded
     $timeout(function() {
         var birds = document.getElementsByClassName("pajaro");
         angular.forEach(birds, function(bird) {
           bird.classList.add('hide');
         });
     }, 0)
+
+    $scope.currentDate = new Date().getDay();
+
+    // key, example of the API call
     $scope.key = '886705b4c1182eb1c69f28eb8c520e20';
     $scope.example = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&APPID=' + $scope.key;
     $scope.city = cityService.city;
@@ -16,21 +22,22 @@ angular.module('weatherApp').controller('homeController', ['$scope','cityService
 
     $http.get($scope.example)
     .success(function(response) {
-        console.log(response);
-        console.log('Main controller working')
         $scope.daysTemperatures = response.list;
     })
     .error(function(e) {
         console.log(e);
     })
 
+    // If you want to change the API Values
+    // Resource helps if you want to change the arguments q: or cnt: in angularJS
     $scope.weatherAPI =
     $resource($scope.example, 
         {
-        callback: "JSON_CALLBACK" 
+            callback: "JSON_CALLBACK" 
         },
         {
-        get: { method: "JSONP" }
+            get: { method: "JSONP" }
         });
     $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: 2});
+
 }])
